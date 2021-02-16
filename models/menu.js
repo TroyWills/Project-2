265ae11d0033ddfefb84aaacf26e64e1677da4fb
Hayden Garrard <hagarrard@gmail.com>
2021-02-16 15:02:27 -0600

    finished table via bulk create

--- models/menu.js
+++
@@ -1,9 +0,0 @@
-barOverview.bulkCreate([
-  { drink_name: 'Tequila Sour', price: true,description: 'fun tequila' },
-//   { drink_name: 'Rum and Coke', price: true },
-//   { drink_name: 'bar', price: false }
-]).then(() => { // Notice: There are no arguments here, as of right now you'll have to...
-  return barOverview.findAll();
-}).then(response => {
-  console.log(response) // ... in order to get the array of user objects
-})
