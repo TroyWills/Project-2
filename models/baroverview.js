63df07d98aab5a6584db35024732a3a618676ee2
Troy Wills <you@example.com>
2021-02-16 11:33:09 -0600

    deploy to Heroku & create menu table headers

--- models/baroverview.js
+++ models/baroverview.js
@@ -1,4 +1,23 @@
-// CREATE MODEL for drink menu 
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
 
+var menu = sequelize.define("menu", {
+  drink_name: Sequelize.STRING,
+  price: Sequelize.DECIMAL(10,2),
+  description: Sequelize.STRING
+});
+(async () => {
+    await sequelize.sync({ force: true });
+    sequelize
+    .authenticate()
+    .then(() => {
+      console.log("table was successfully created");
+    })
+    .catch(err => {
+      console.error('error:', err);
+    });
+  })();
 
-// Add data to drink menu model 
+menu.sync();
+
+module.export = menu;
