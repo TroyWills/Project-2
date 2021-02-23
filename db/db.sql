351fe678ca5d5a10e15d1e2e4d2ffbb82bd53e9b
Troy Wills <you@example.com>
2021-02-23 15:28:28 -0600

    created seeds.sql file for Heroku deploy

--- db/db.sql
+++ db/db.sql
@@ -2,3 +2,17 @@ DROP DATABASE IF EXISTS baroverview
 create DATABASE baroverview;
 
 use baroverview;
+
+CREATE TABLE Menu (
+id INT NOT NULL AUTO_INCREMENT,
+drink_name VARCHAR(255),
+price DECIMAL(10,2),
+description VARCHAR(255),
+ingredients VARCHAR(255),
+category VARCHAR(255),
+images VARCHAR(255),
+PRIMARY KEY(id)
+)
+    
+DROP TABLE Menu
+SELECT * FROM Menu
---
+++ db/seeds.sql
@@ -0,0 +1,80 @@
+ INSERT INTO Menu (drink_name, price, description, ingredients, category, images) values
+("Tequila Sour", 
+ 13.00, 
+ "A tangy and refreshing must have after a long day!", 
+ "Your favorite tequila, lime juice,lemon juice, agave syrup, angostura bitters, and egg white.", 
+ "cocktail",
+ "https://www.acouplecooks.com/wp-content/uploads/2020/04/Tequila-Sour-012.jpg"
+),
+(
+'Rum and Coke', 
+10.00, 
+'Kick back with our simple yet satisfying rum and coke.', 
+'Rum, Coca-cola, and a lime wedge.', 
+'cocktail',
+'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190904-rum-and-coke-0345-landscape-pf-1568846706.jpg'
+),
+( 
+'S.H.O.T.S', 
+8.00, 
+'Take a load off with some shots.', 
+'Your choice of any of our delectable liquors.', 
+'hard-liquor',
+'https://i.pinimg.com/originals/95/16/84/951684152a3dea017f196822e1b14a4f.jpg'
+),
+( 
+'Long Island Iced Tea', 
+11.00, 
+'A cocktail that will never go out of style! The concoction of rum, vodka, gin, tequila, and triple sec is irrestiable. ' , 
+'Your choice of any of our delectable liquors.', 
+'cocktail',
+'https://www.thespruceeats.com/thmb/OfagHnZQpgIs4Y0ww-h2SsL_s8g=/889x667/smart/filters:no_upscale()/LIT-5a8f365efa6bcc00372a7634.jpg'
+),
+( 
+'Craft Beer', 
+7.00, 
+'Try any of our delicious craft beers as well as import beers as well.' , 
+'Beer with a choice of a garnish if you would like.', 
+'beer',
+'https://beveragedynamics.com/wp-content/uploads/2018/09/iStock-923450642.jpg'
+),
+( 
+'Dry Martini', 
+14.00, 
+'Our famous dry martini that has stood the test of time.' , 
+'Gin, dry vermouth, orange bitters, with a lemon twist garnish.', 
+'martini',
+'https://www.liquor.com/thmb/Tg83gGIz-cS8nO-aAHa2N6R4eFQ=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/dry-martini-720x720-primary-a6de08f8cd584ad88520287922578bcb.jpg'
+),
+( 
+'Mojito', 
+12.00, 
+'A fresh taste of summer, year round!' , 
+'White rum, club soda, mint leaves, lime, and sugar.', 
+'cocktail',
+'https://cdn.loveandlemons.com/wp-content/uploads/2020/07/mojito-500x500.jpg'
+),
+( 
+'Mai Tai', 
+12.50, 
+'This Polynesian-style cocktail has been the symbol of Tahitian culture ever since the drink was created.' , 
+'Light rum, gold rum, Orange Curacao liqueur, almond syrup, lime juice, and dark rum.', 
+'cocktail',
+'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/refreshing-rum-mai-tai-cocktail-royalty-free-image-1129485781-1567157013.jpg?crop=0.664xw:0.498xh;0.154xw,0.361xh&resize=1200:*'
+),
+( 
+'Wine', 
+15.00, 
+'Choose from our many house wines.' , 
+'Wine.', 
+'wine' ,
+'https://cdn.shopify.com/s/files/1/0156/2938/5792/products/product-image-1287441452.jpg?v=1583314092'
+),
+(
+'PBR ', 
+4.00, 
+'As requested from the famous Matty J, the famous (or infamous) PBR!' , 
+'A solution to a bad day in a can.', 
+'classic drink',
+'https://media3.s-nbcnews.com/j/newscms/2018_46/2643221/181112-pbr-al-1054_2e4a5b33a86c258df79f0a21e527dd91.nbcnews-fp-1200-630.jpg'
+);
